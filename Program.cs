6fb4e8360bb9f8e2a6c99794a94c406d86517cd7
Moises Alves <moisesalves1@gmail.com>
2022-12-14 00:27:03 -0300

    Working with files and directories

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("Tailwind\\stores");
+
+foreach (var dir in listOfDirectories) {
+    Console.WriteLine(dir);
+}
+
+// Outputs:
+// stores/201
+// stores/202
+
+Console.WriteLine("--------------------------------------------");
+
+IEnumerable<string> files = Directory.EnumerateFiles("Tailwind\\stores");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+// Outputs:
+// stores/totals.txt
+// stores/sales.json
+
+Console.WriteLine("--------------------------------------------");
+// Find all *.txt files in the stores folder and its subfolders
+IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("Tailwind\\stores", "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in allFilesInAllFolders)
+{
+    Console.WriteLine(file);
+}
+
+// Outputs:
+// stores/totals.txt
+// stores/201/inventory.txt
